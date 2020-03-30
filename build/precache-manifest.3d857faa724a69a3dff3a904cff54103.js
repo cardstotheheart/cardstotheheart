bdacbfb1e05435c7d703262bb8fc07b4d7028b08
cardstotheheart@gmail.com <36455793+domisjustanumber@users.noreply.github.com>
2020-03-29 20:26:40 -0400

    Node build

---
+++ build/precache-manifest.3d857faa724a69a3dff3a904cff54103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda7c705e73b2e4698e7ebf2c9a55c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "420473fec14893fbe8ae",
+    "url": "/static/css/main.c7ce1437.chunk.css"
+  },
+  {
+    "revision": "968003acfed50a94c812",
+    "url": "/static/js/2.045405f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.045405f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420473fec14893fbe8ae",
+    "url": "/static/js/main.c6472db0.chunk.js"
+  },
+  {
+    "revision": "2737cab1dcf5c07ab300",
+    "url": "/static/js/runtime-main.159c8ac7.js"
+  }
+]);
